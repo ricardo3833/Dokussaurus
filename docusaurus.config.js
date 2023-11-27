8b0a1da38cf5b6848f8e405845cf481870577c80
Ricardo Batista <ricardobatista@Ricardos-MacBook-Pro.local>
2023-11-27 15:33:17 +0000

    Configuração inicial do site

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -19,7 +19,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'ricardo3822', // Usually your GitHub org/user name.
-  projectName: 'dokussaurus', // Usually your repo name.
+  projectName: 'Dokussaurus', // Usually your repo name.
   deploymentBranch: 'gh-pages',
 
   onBrokenLinks: 'throw',
